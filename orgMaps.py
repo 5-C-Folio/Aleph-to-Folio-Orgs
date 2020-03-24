baf1e40c8e5b7ec619959b73996c9f1855156bcf
Aaron Neslin <aneslin125@gmail.com>
2020-03-24 09:47:41 -0400

    spelling errors

--- orgMaps.py
+++ orgMaps.py
@@ -3,6 +3,14 @@ import json
 import uuid
 from marshSchema import phoneRecord, urlRecord, organizationRecord , addressRecord, emailRecord
 
+def categories_getter(value):
+    #currently is only designed to have type 1 or 3 in the export file. Should be controlled in Z72_reader function
+    if value["Z72_REC_KEY 2"] == "1":
+        categories = ["UUIDplaceholder1"]
+    elif value["Z72_REC_KEY 2"] == "3":
+        categories = ["618084d5-57ed-49a8-8e91-bae476e1d247"]
+    return categories
+
 
 def orgmaker(z70, z72, prefix):
     jsonRecords = []
@@ -49,15 +57,14 @@ def addressNoteGetter(z72_file):
                 addLine = row['Z72_NOTE']
                 export_list.append(addLine)
         return("""
-        """.join(export_list))
+ """.join(export_list))
 
 def addressMaker(z72_file):
         export_list = []
         for value in z72_file:
-                if value["Z72_REC_KEY 2"] == "1":
-                    categories = ["placeholderUUID1"]
-                elif value["Z72_REC_KEY 2"] == "3":
-                        categories = ["placeholderUUID3"]
+
+                categories = categories_getter(value)
+
                 addLine = addressRecord(addressLine1 = value["Z72_VENDOR_ADDRESS"],
                                     city = value['Z72_VENDOR_CITY'],
                                     zipCode = value['Z72_VENDOR_POSTAL_CODE'],
@@ -69,27 +76,21 @@ def addressMaker(z72_file):
         print(export_list)
         return(export_list)
 
-def catagories_getter(value):
-    if value["Z72_REC_KEY 2"] == "1":
-        categories = ["placeholderUUID1"]
-    elif value["Z72_REC_KEY 2"] == "3":
-        categories = ["placeholderUUID3"]
-    return categories
 
 
 
 def  phoneMaker(z72_file):
             phonelist = []
             for value in z72_file:
-                catagories = catagories_getter(value)
+                categories = categories_getter(value)
                 if value['Z72_VENDOR_TEL']:
                         phoneMain = (value['Z72_VENDOR_TEL'])
-                        phone1 = phoneRecord(phoneNumber=phoneMain, type='Office', categories = catagories)
+                        phone1 = phoneRecord(phoneNumber=phoneMain, type='Office', categories = categories)
                         if len(phone1["phoneNumber"]) >1:
                             phonelist.append(phone1)
                 if value['Z72_VENDOR_FAX']:
                         phoneFax = value['Z72_VENDOR_FAX']
-                        phone2 = phoneRecord(phoneNumber=phoneFax, type='Fax', categories = catagories)
+                        phone2 = phoneRecord(phoneNumber=phoneFax, type='Fax', categories = categories)
                         if len(phone2["phoneNumber"]) >1:
                             phonelist.append(phone2)
             return phonelist
@@ -98,8 +99,8 @@ def  phoneMaker(z72_file):
 def emailMaker(z72_file):
                 exportList = []
                 for value in z72_file:
-                    catagories = catagories_getter(value)
-                    email = emailRecord(value=value['Z72_VENDOR_EM'], categories = catagories)
+                    categories = categories_getter(value)
+                    email = emailRecord(value=value['Z72_VENDOR_EM'], categories = categories)
                     if len(email["value"]) > 1:
                         exportList.append(email)
                 return exportList
@@ -118,7 +119,7 @@ def uuidgen(inst, code):
 
 if __name__ == "__main__":
 
-    x = orgmaker("orgs//Rnd2 orgs//HAM_Z70_RAW.txt", "orgs//Rnd2 orgs//Cleaned Z72//HAM_Z72_Cleaned_V1.csv",  "MH")
+    x = orgmaker("orgs//Rnd2 orgs//MHC_Z70_RAW.txt", "orgs//Rnd2 orgs//Cleaned Z72//MHC_Z72-Cleaned_V2.csv",  "MH")
     with open("testfile.txt", 'w', encoding='latin-1', newline="\n") as target:
         json.dump(x,target, indent=4)
 
