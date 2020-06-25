77ccd6e55d74bf0a1654cdbc4b00bcb975ece28a
Aaron Neslin <aneslin125@gmail.com>
2020-06-25 12:40:05 -0400

    added code adding and different handling of alias splits

--- coral_orgs.py
+++ coral_orgs.py
@@ -5,22 +5,25 @@ from sys import argv
 
 def string_to_dict(string):
     #todo smith uses , to split- umass ;
+    print(string)
     sp_string = string.split(';')
+    print(sp_string)
     altList = []
     for row in sp_string:
         sprow = row.split(':')
+        print (sprow)
         altname = aliasRecord( value=sprow[1].lstrip(), description=sprow[0])
         altList.append(altname)
     return altList
 
 
-def coral_getter(contact_file, orgCode):
+def coral_getter(contact_file, orgCode, code):
     c_list = []
     with open (contact_file, 'r') as c_file:
         c_dict = DictReader(c_file)
         for row in c_dict:
             try:
-                if row['orgCode'] == orgCode:
+                if row['orgCode'] == code + orgCode:
                     if len(row['ID']) > 1:
                         c_list.append(row["ID"])
             except KeyError:
@@ -30,15 +33,15 @@ def coral_getter(contact_file, orgCode):
         return c_list
 
 
-def coral_create(main_file, contact_file, interface_file):
+def coral_create(main_file, contact_file, interface_file, code):
 
     with open(main_file, 'r', encoding='utf8') as dictfile:
         coral_orgs = DictReader(dictfile)
         recList = []
         for org in coral_orgs:
-            c_list = coral_getter(contact_file, org['orgCode'])
+            c_list = coral_getter(contact_file, org['orgCode'], code)
             if interface_file.lower() not in ["null", 'n', 'none']:
-                i_list = coral_getter(interface_file, org['orgCode'])
+                i_list = coral_getter(interface_file, org['orgCode'], code)
             else:
                 i_list = []
             print (f"{len(c_list)} contact created and {len(i_list)} interfaces created for {org['name']}")
@@ -48,8 +51,8 @@ def coral_create(main_file, contact_file, interface_file):
                 isVen = False
             newRec = organizationRecord(
                                         id=None,
-                                        code=org['orgCode'].replace(" ", ""),
-                                        name=org['name'],
+                                        code=code + org['orgCode'].replace(" ", ""),
+                                        name=code + ' ' + org['name'],
                                         status='Active',
                                         description=org['noteText'],
                                         isVendor=isVen,
@@ -82,7 +85,7 @@ if __name__ == '__main__':
     #coral = coral_create('orgs\\Coral\\smith_organizations-Cleaned-v1.csv','orgs/Contacts/sc_contacts_v1_FOLIO_IDs.csv',
                          #'orgs/Coral/sc_interfaces_v2_FOLIO_IDs.csv')
     if conf in ["yes", "Yes", "Y", 'y']:
-        coral = coral_create(coralFile, contactsFile, interfaceFile)
+        coral = coral_create(coralFile, contactsFile, interfaceFile, code)
         try:
             with open(f"{code}final//{code}_coralfile.txt", 'w', encoding='latin-1', newline="\n") as target:
                 json.dump(coral, target, indent=4)
